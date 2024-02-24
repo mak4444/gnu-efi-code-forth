0641d5e27360283a264b6087758169cbce6350d2
mak4444 <fpauk@mail.ru>
2024-02-24 17:48:11 +0300

    Add files via upload

--- qemuboot.sh
+++ qemuboot.sh
@@ -1,4 +1,4 @@
 #sudo apt -y install ovmf
 #https://github.com/queso-fuego/UEFI-GPT-image-creator - ./bios64_app.bin ./bios64.bin
 #qemu-system-x86_64 -enable-kvm -bios ./bios64_app.bin -hda fat:rw:./disk_nmt
-qemu-system-x86_64 -enable-kvm -bios /usr/share/ovmf/OVMF.fd  -hda fat:rw:./disk_nmt
+qemu-system-x86_64 -enable-kvm -bios /usr/share/ovmf/OVMF.fd  -hda fat:rw:./disk_nmt -hdb fat:rw:./lib
